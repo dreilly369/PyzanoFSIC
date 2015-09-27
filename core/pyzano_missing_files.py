8371402c0f8226b97d0fb1a256c5d6dd23161cb2
dreilly369 <executivewd@hotmail.com>
2015-09-27 16:49:02 -0700

    multiple fixes
    Fixed bug where missing file search ignored starting location by adding a like clause to the sql WHERE statement.
    Added check for HOSTNAME to file lookup
    Added output text for actions
    Fixed bug where Pyzano failed to create a nested directory for a missing file

--- core/pyzano_missing_files.py
+++ core/pyzano_missing_files.py
@@ -1,15 +1,20 @@
+import os.path
 #! /usr/bin/python
 import os
 import json
 import re
 import binascii
+import subprocess
 
 from pyzano_mysql import DBManager
 
 class MissingFileScanner:
     disposition = 'i' # i = interactive, r = restore from DB, d = delete from DB
-    def __init__(self,disposition='i'):
+    topDir = ""
+    
+    def __init__(self,startAt="/",disposition='i'):
         self.disposition = disposition
+        self.topDir = startAt
         
     def load_config(self):
         base_dir = os.path.dirname(os.path.dirname(__file__))
@@ -18,8 +23,12 @@ class MissingFileScanner:
         return json.load(open(conf_loc,"r"))
 
     def scanForRemoved(self):
-        dbm = DBManager(self.load_config())
-        files = dbm.readFromDatabase("file_location,id","1")
+        conf = self.load_config()
+        dbm = DBManager(conf)
+        wHost = "host_name=\"%s\" AND " % conf["HOSTNAME"]
+        wLoc = "file_location LIKE \"%s" % self.topDir
+        wLoc += "%\""
+        files = dbm.readFromDatabase("file_location,id", wHost + wLoc)
         missing_files = []
         for file_row in files:
             theF = re.sub(r'\\(.)', r'\1',file_row[0])
@@ -28,24 +37,30 @@ class MissingFileScanner:
         for fpack in missing_files:
             row_id = fpack[0]
             if self.disposition is "i":
-                choice = self.handleMissingInteractive(row_id,theF)
+                choice = self.handleMissingInteractive(theF)
             else:
                 choice = self.disposition
                 
             if choice is "d":
+                print "removing from db"
                 self.handleDeleteFromDB(row_id)
             elif choice is "r":
+                print "restoring to %s" % theF
                 self.handleRevertFromDb(row_id)
+            else: 
+                print "skipping"
+                pass
+                
         return missing_files
     
-    def handleMissingInteractive(self,row_id,fn):
+    def handleMissingInteractive(self,fn):
         choice = ""
         while choice.lower() not in ["delete","revert","skip","d","r","s"]:
             print "How would you like to handle %s?:" % fn
-            choice = raw_input("(d)elete from DB, (r)evert from DB, or (s)kip: ")
+            choice = raw_input("(d)elete from DB, (r)estore from DB, or (s)kip: ")
         if choice in ["d","delete"]:
             return 'd'
-        elif choice in ["r","revert"]:
+        elif choice in ["r","restore"]:
             return 'r'
         else:
             return 's'
@@ -64,6 +79,13 @@ class MissingFileScanner:
             return False
         data = binascii.unhexlify(rows[0][1])
         local = re.sub(r'\\(.)', r'\1',rows[0][2])
+        container = os.path.dirname(rows[0][2])
+        if not os.path.isdir(container):
+            call = "mkdir -p %s" % container
+            print call
+            subprocess.call(call)
+        
         with open(local,"wb") as f:
             f.write(data)
             f.close()
+        print "%s was restored" % rows[0][2]
